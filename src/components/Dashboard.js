61567a34edc8ce7178824f9c33d6d5c3f51da062
ariandto <92358210+ariandto@users.noreply.github.com>
2024-07-04 12:39:14 +0700

    oke

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -150,9 +150,6 @@ function Dashboard() {
         <div className="chart-container">
           <Bar data={chartData} />
         </div>
-
-        {/* Dashboard Informa */}
-        <Dashboardinforma />
       </div>
     </div>
   );
