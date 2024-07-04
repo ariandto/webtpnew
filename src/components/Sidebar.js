4fe2436c8e2a31d11524574220f52dbc9038bf99
ariandto <92358210+ariandto@users.noreply.github.com>
2024-07-04 12:18:32 +0700

    up sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { FaBars, FaTimes, FaHome, FaChartBar, FaLaptopCode, FaChartLine, FaCity } from 'react-icons/fa';
+import { FaBars, FaTimes, FaHome, FaChartBar, FaChartLine, FaCity } from 'react-icons/fa';
 import './Sidebar.css';
 import Dashboard from './Dashboard';
 import Areakirim from './Areakirim';
+import Dashboardinforma from './Dashboardinforma';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,7 +18,7 @@ function App() {
       <div className="app">
         <div className={`sidebar ${isOpen ? 'open' : 'collapsed'}`}>
           <div className="sidebar-header">
-            <h3>{isOpen ? 'Transport Planning KLS' : ''}</h3>
+            <h3>{isOpen && 'Transport Planning KLS'}</h3>
             <button className="sidebar-toggle" onClick={toggleSidebar}>
               {isOpen ? <FaTimes /> : <FaBars />}
             </button>
@@ -30,59 +31,38 @@ function App() {
               <Link to="/" className="menu-item">
                 <FaHome />
                 {isOpen && <span className="menu-text">Home</span>}
-                {!isOpen && (
-                  <span className="tooltip">
-                    <span className="tooltip-icon"><FaHome /></span>
-                    Home
-                  </span>
-                )}
+                <span className="tooltip">Home</span>
               </Link>
             </li>
             <li>
               <Link to="/dashboard" className="menu-item">
                 <FaChartBar />
                 {isOpen && <span className="menu-text">Dashboard LC</span>}
-                {!isOpen && (
-                  <span className="tooltip">
-                    <span className="tooltip-icon"><FaChartBar /></span>
-                    Dashboard
-                  </span>
-                )}
+                <span className="tooltip">Dashboard LC</span>
               </Link>
             </li>
             <li>
-            <Link to="/areakirim" className="menu-item">
+              <Link to="/areakirim" className="menu-item">
                 <FaCity />
                 {isOpen && <span className="menu-text">Area Kirim</span>}
-                {!isOpen && (
-                  <span className="tooltip">
-                    <span className="tooltip-icon"><FaCity  /></span>
-                    Area Kirim
-                  </span>
-                )}
-                </Link>
+                <span className="tooltip">Area Kirim</span>
+              </Link>
             </li>
             <li>
-              <a href="#" className="menu-item">
+              <Link to="#" className="menu-item">
                 <FaChartLine />
                 {isOpen && <span className="menu-text">Reporting</span>}
-                {!isOpen && (
-                  <span className="tooltip">
-                    <span className="tooltip-icon"><FaChartLine /></span>
-                    Reporting
-                  </span>
-                )}
-              </a>
+                <span className="tooltip">Reporting</span>
+              </Link>
             </li>
           </ul>
-          <div className="profile-section">
-            {isOpen && (
+          {isOpen && (
+            <div className="profile-section">
               <div className="profile-details">
-                {/* <p className="profile-name">Your Name</p>
-                <p className="profile-role">Your Role</p> */}
+                {/* Add user details here */}
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
         <div className="content">
           <Routes>
@@ -93,7 +73,8 @@ function App() {
               </div>
             } />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/areakirim" element={<Areakirim/>} />
+            <Route path="/areakirim" element={<Areakirim />} />
+            <Route path="/dashboardinforma" element={<Dashboardinforma />} />
           </Routes>
         </div>
       </div>
